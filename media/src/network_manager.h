228958d3e4132f2901f09a28622c912dbb8a856b
jiegeaiai <jiegeaiai@163.com>
2021-12-04 13:35:12 +0800

    change modiyf

---
+++ media/src/network_manager.h
@@ -0,0 +1,38 @@
+#ifndef src_input_input_medai_source_h_
+#define src_input_input_medai_source_h_
+
+#include <memory>
+#include <list>
+#include <string>
+
+#include "src/media_source_manager.h"
+#include "src/media_source_factory.h"
+
+namespace LJMP {
+    namespace Input{
+
+        class InputMediaSourceManager : public MediaSourceManager {
+            disable_copy(InputMediaSourceManager)
+            
+        public:
+            static std::shared_ptr<InputMediaSourceManager> create();
+
+        public:
+            ~InputMediaSourceManager() override;
+            
+            bool initialize() override;
+            void uninitialize() override;
+            
+            bool open(const std::string& url) override;
+            
+        protected:
+            InputMediaSourceManager();
+
+        private:
+            using MediaSourceFactoryList = std::list<MediaSourceFactoryPtr >;
+            MediaSourceFactoryList media_source_factory_;
+        };
+    }
+}
+
+#endif // !src_input_input_medai_source_h_
