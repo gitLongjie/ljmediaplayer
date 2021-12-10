5ad6176a84656bf783cc296f41c6e70c10a570b5
longjie1 <TAL@DESKTOP-5EM4FG4>
2021-12-10 11:17:18 +0800

    update media context

--- media/src/media_context.cpp
+++ media/src/media_context.cpp
@@ -23,7 +23,26 @@ namespace LJMP {
     }
 
     void MediaContext::updateMediaConfig(const MediaConfig& config) {
+        if (isCurrentThread()) {
+            onUpdateMediaConfig(config, shared_from_this());
+            return;
+        }
 
+        WPtr self(shared_from_this());
+        auto task = createTask(std::bind(&MediaContext::onUpdateMediaConfig, this, config, self));
+        invoke(task);
+    }
+
+    void MediaContext::onUpdateMediaConfig(const MediaConfig config, WPtr wThis) {
+        LOG_ENTER;
+
+        TaskQueueObject::Ptr self(wThis.lock());
+        if (!self) {
+            LOGE("{} is destructed", (long long)this);
+            return;
+        }
+
+        media_config_ = config;
     }
 
 }
