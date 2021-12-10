dc1fc60d4b090dd6fcbca9f9ca7fda6fc81de5e0
longjie1 <TAL@DESKTOP-5EM4FG4>
2021-12-10 21:10:04 +0800

    add media source channel

---
+++ media/src/media_source_channel.cpp
@@ -0,0 +1,21 @@
+#include "src/media_source_channel.h"
+
+#include "src/log.h"
+#include "src/media_source.h"
+
+namespace LJMP {
+    MediaSourceChannel::Ptr create(const MediaSource::Ptr& media_source, const TaskQueue::Ptr& task_queue) {
+        return createPtr<MediaSourceChannel>(media_source, task_queue);
+    }
+
+    MediaSourceChannel::MediaSourceChannel(const MediaSource::Ptr& media_source, const TaskQueue::Ptr& task_queue)
+        : MediaChannel(task_queue)
+        , media_source_(media_source) {
+
+    }
+
+    MediaSourceChannel::~MediaSourceChannel() {
+
+    }
+
+}
---
+++ media/src/media_source_channel.h
@@ -0,0 +1,31 @@
+#ifndef src_media_source_channel_h_
+#define src_media_source_channel_h_
+
+#include <memory>
+
+#include "src/media_channel.h"
+
+namespace LJMP {
+    class MediaSource;
+
+    class MediaSourceChannel : public MediaChannel {
+        disable_copy(MediaSourceChannel)
+
+    public:
+        using Ptr = std::shared_ptr<MediaSourceChannel>;
+        static Ptr create(const std::shared_ptr<MediaSource>& media_source, const TaskQueue::Ptr& task_queue);
+
+    public:
+        ~MediaSourceChannel() override;
+
+    protected:
+        MediaSourceChannel(const std::shared_ptr<MediaSource>& media_source,
+            const TaskQueue::Ptr& task_queue);
+
+    private:
+        std::shared_ptr<MediaSource> media_source_;
+    };
+}
+
+#endif // !src_media_source_channel_h_
+
